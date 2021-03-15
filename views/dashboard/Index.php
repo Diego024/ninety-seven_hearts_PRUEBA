78d6d7049fd04e97066a6649a6d0912c8873889c
Diego Pacheco <diego.pacheco020603@gmail.com>
2021-03-15 13:28:15 -0600

    Agregando el sidebar del dashboard

--- views/dashboard/Index.php
+++ views/dashboard/Index.php
@@ -1,131 +1,102 @@
 <!doctype html>
 <html lang="en">
- 
-    <head>
- 
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
-        <title>Bootstrap 4 Template with Sidebar Menu</title>
- 
-        <!-- Style -->
-        <link rel="stylesheet" href="../../resources/styles/css/dashboard/Index.css">
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500&display=swap">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.min.css">
-        <link rel="stylesheet" href="assets/css/animate.css">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="stylesheet" href="assets/css/media-queries.css">
- 
-        <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.png">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
- 
-    </head>
- 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Dashboard</title>
+    <!-- Style -->
+    <link rel="stylesheet" href="../../resources/styles/css/dashboard/Index.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
     <body>
 
-    <!-- Wrapper -->
-    <div class="wrapper">
-    
+    <div class="d-flex " id="wrapper">
         <!-- Sidebar -->
-        <nav class="sidebar">
-    
-            <!-- close sidebar menu -->
-            <div class="dismiss">
-                <i class="fas fa-arrow-left"></i>
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="sidebar--img">
+                <img src="https://www.howdeniberia.com/wp-content/uploads/2018/05/Disney-logo-png-transparent-download.png" alt="" id="sidebar--img__logo">
             </div>
-    
-            <div class="logo">
-                <h3><a href="index.html">Bootstrap 4 Template with Sidebar Menu</a></h3>
+            <div class="sidebar--separator">
+                <div class="sidebar--separator__line"></div>
             </div>
-    
-            <ul class="list-unstyled menu-elements">
-                <li class="active">
-                    <a class="scroll-link" href="#top-content"><i class="fas fa-home"></i> Home</a>
-                </li>
-                <li>
-                    <a class="scroll-link" href="#section-1"><i class="fas fa-cog"></i> What we do</a>
-                </li>
-                <li>
-                    <a class="scroll-link" href="#section-2"><i class="fas fa-user"></i> About us</a>
-                </li>
-                <li>
-                    <a class="scroll-link" href="#section-5"><i class="fas fa-pencil-alt"></i> Portfolio</a>
-                </li>
-                <li>
-                    <a class="scroll-link" href="#section-6"><i class="fas fa-envelope"></i> Contact us</a>
-                </li>
-                <li>
-                    <a href="#otherSections" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" role="button" aria-controls="otherSections">
-                        <i class="fas fa-sync"></i>Other sections
-                    </a>
-                    <ul class="collapse list-unstyled" id="otherSections">
-                        <li>
-                            <a class="scroll-link" href="#section-3">Our projects</a>
-                        </li>
-                        <li>
-                            <a class="scroll-link" href="#section-4">We think that...</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-    
-            <div class="to-top">
-                <a class="btn btn-primary btn-customized-3" href="#" role="button">
-                    <i class="fas fa-arrow-up"></i> Top
-                </a>
+            <div class="sidebar--info__container">
+                <div class="sidebar--info__img">
+                    <img src="../../resources/statics/images/profile-cuadrado.jpg" alt="" id="info--img__user">
+                </div>
+                <div class="sidebar--info__data">
+                    <p class="sidebar--info__text">Bienvenido,</p>
+                    <p class="sidebar--info__name">Diego Pacheco</p>
+                </div>
             </div>
-    
-            <div class="dark-light-buttons">
-                <a class="btn btn-primary btn-customized-4 btn-customized-dark" href="#" role="button">Dark</a>
-                <a class="btn btn-primary btn-customized-4 btn-customized-light" href="#" role="button">Light</a>
+            <div class="sidebar--separator">
+                <div class="sidebar--separator__line"></div>
             </div>
-    
-        </nav>
-        <!-- End sidebar -->
-    
-        <!-- Dark overlay -->
-        <div class="overlay"></div>
-    
-        <!-- Content -->
-        <div class="content">
-    
-            <!-- open sidebar menu -->
-            <a class="btn btn-primary btn-customized open-menu" href="#" role="button">
-                <i class="fas fa-align-left"></i> <span>Menu</span>
-            </a>
-    
-            <!-- ... -->
-    
-            <!-- here is the page's content (not shown here) -->
-    
-            <!-- ... -->
-    
+            <div class="list-group list-group-flush">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link btn-block text-left btn--header__texto" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Mantenimientos
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Dashboard</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Shortcuts</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Overview</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Events</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Profile</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Status</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link btn-block text-left collapsed btn--header__texto" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    Datos
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Events</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Profile</a>
+                                <a href="#" class="list-group-item list-group-item-action btn--boton__texto">Status</a>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" id="logout--button" class="list-group-item list-group-item-action btn--boton__texto">Status</a>
+                </div>
+            </div>
         </div>
-        <!-- End content -->
+        <!-- /#sidebar-wrapper -->
     
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+    
+            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+            </nav>
+        </div>
+        <!-- /#page-content-wrapper -->
     </div>
-    <!-- End wrapper -->
-
-    <!-- Javascript -->
-    <script src="assets/js/jquery-3.3.1.min.js"></script>
-        <script src="assets/js/jquery-migrate-3.0.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="assets/js/jquery.backstretch.min.js"></script>
-        <script src="assets/js/wow.min.js"></script>
-        <script src="assets/js/jquery.waypoints.min.js"></script>
-        <script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
- 
+    
+    <!-- Bootstrap Bundle -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    
+    <!-- Menu Toggle Script -->
+    <script>
+        $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        });
+    </script>
+    
     </body>
- 
 </html>
